b0c0bf1252c0be33461242a464ad75d84da8e838
Terry Jackson <tjackso74492@yahoo.com>
2015-06-20 17:49:43 +0000

    Added profiles new action

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users do
-    resources :profile
-  end 
+   resource :profile
+  end
   resources :contacts
   get '/about' => 'pages#about'
   root'pages#home'
