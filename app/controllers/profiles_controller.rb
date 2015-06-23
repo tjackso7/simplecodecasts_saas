9ad4e89cf0ed9b3af48bc6586e00d117a95d9c90
Terry Jackson <tjackso74492@yahoo.com>
2015-06-23 17:11:39 +0000

    Added users show action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,7 +9,7 @@ class ProfilesController < ApplicationController
     @profile = @user.build_profile(profile_params)
     if @profile.save
       flash[:success] = "Profile Updated!"
-      redirect_to user_path( parms[:user_id] )
+      redirect_to user_path( params[:user_id] )
     else 
       render action: :new
     end
